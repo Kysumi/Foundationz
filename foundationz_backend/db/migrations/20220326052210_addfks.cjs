f61c5ce4911c7fd45e4016b47c56f4b0c02700b8
Maunder <maunderscott@gmail.com>
2022-03-27 14:25:54 +1300

    Adding migration

--- foundationz_backend/db/migrations/20220326052210_addfks.cjs
+++ foundationz_backend/db/migrations/20220326052210_addfks.cjs
@@ -1,11 +1,13 @@
 
 exports.up = function(knex) {
     return knex.schema.alterTable('organization_user', table => {
-        table.uuid("organization_id");
+        table.uuid("organization_id")
+            .notNullable();
         table.foreign('organization_id')
             .references('organizations.id');
 
-        table.uuid("user_id");
+        table.uuid("user_id")
+            .notNullable();
         table.foreign('user_id')
             .references('users.id');
     })
---
+++ foundationz_backend/db/migrations/20220326094535_arc_table_setup.cjs
@@ -0,0 +1,72 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("resources", table => {
+        table.uuid("id")
+            .primary()
+            .notNullable();
+        table.timestamps(false, true);
+
+        table.uuid("user_id");
+        table.uuid("location_id");
+        table.uuid("employee_id");
+    })
+        .raw(`
+            alter table "resources"
+            add constraint "exactlyOneResourceType" check(
+                ("user_id" is not null and "location_id" is null and "employee_id" is null)
+                or
+                ("user_id" is null and "location_id" is not null and "employee_id" is null)
+                or
+                ("user_id" is null and "location_id" is null and "employee_id" is not null)
+            )
+        `)
+        .createTable("projects", table => {
+            table.uuid("id")
+                .primary()
+                .notNullable();
+            table.string("name");
+            table.timestamps(false, true);
+        })
+        .createTable("project_resource", table => {
+            table.increments("id")
+                .primary();
+            table.timestamps(false, true);
+
+            table.uuid("project_id")
+                .notNullable();
+            table.uuid("resource_id")
+                .notNullable()
+
+            table.foreign('resource_id')
+                .references('resources.id');
+            table.foreign('project_id')
+                .references('projects.id');
+        })
+        .createTable("locations", table => {
+            table.uuid("id")
+                .primary()
+                .notNullable();
+
+            table.timestamps(false, true);
+        })
+        .createTable("employees", table => {
+            table.uuid("id")
+                .primary()
+                .notNullable();
+
+            table.timestamps(false, true);
+            table.string("name");
+        })
+        .alterTable("resources", (table) => {
+            table.foreign('user_id')
+                .references('users.id');
+            table.foreign('location_id')
+                .references('locations.id');
+            table.foreign('employee_id')
+                .references('employees.id');
+        });
+};
+
+exports.down = function (knex) {
+
+};
+
