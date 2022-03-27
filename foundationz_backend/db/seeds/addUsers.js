ec4f24342bb2d78ae6b656fb47e991d9db14b802
Maunder <maunderscott@gmail.com>
2022-03-27 17:48:54 +1300

    Adding constant user for auth testing

--- foundationz_backend/db/seeds/addUsers.js
+++ foundationz_backend/db/seeds/addUsers.js
@@ -12,6 +12,15 @@ export const seed = async (knex) => {
 
     // Deletes ALL existing entries
     await knex('users').del()
+    await knex('users').insert([
+        {
+            id: '7efe2d3a-ace4-11ec-b909-0242ac120002',
+            first_name: `abomy`,
+            surname: `foote`,
+            email: 'aboomy@gmail.com',
+            created_at: new Date()
+        }
+    ]);
     await knex('users').insert(Array.from(Array(5)).map(makeNewFakeUser));
 
     return knex;
