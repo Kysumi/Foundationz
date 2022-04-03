d78c23cad07bc879b0f12517cd05a3a1e78c6dd3
Abomy <abomyy@gmail.com>
2022-04-03 15:58:59 +1200

    Added redis to docker-compose and setup redis store for cookies, sessions now save between restarts

--- foundationz_backend/src/modules/auth/auth.ts
+++ foundationz_backend/src/modules/auth/auth.ts
@@ -1,18 +1,22 @@
-import sessions from "express-session";
+import session from "express-session";
+import redis_connect from "connect-redis";
+import { RedisClient } from "@auth/redis";
 
-export interface SessionData extends sessions.Session {
+//20 minutes
+const LIFETIME = 1000 * 60 * 20;
+const RedisStore = redis_connect(session);
+
+export interface SessionData extends session.Session {
   userid: string;
   email: string;
 }
 
-//20 minutes
-const LIFETIME = 1000 * 60 * 20;
-
-export const configuredSession = () => {
-  return sessions({
+export const configuredSession = async () => {
+  return session({
     name: "foundationz",
     secret: process.env.SESSION_SECRET || "superseceretness",
     saveUninitialized: false,
+    store: new RedisStore({ client: await RedisClient() }),
     cookie: {
       httpOnly: true,
       maxAge: LIFETIME,
