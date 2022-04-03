5331f83893a3786d31c054ae7a485255983cf08f
Abomy <abomyy@gmail.com>
2022-04-03 23:28:53 +1200

    Code clean up to appease the overlord

--- foundationz_backend/src/modules/redis/redis.ts
+++ foundationz_backend/src/modules/redis/redis.ts
@@ -2,7 +2,7 @@ import { createClient, SocketClosedUnexpectedlyError } from "redis";
 
 const REDIS_HOST = process.env.REDIS_HOST || "localhost";
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || "superseceretness";
-let reconnect: any = null;
+let reconnect: NodeJS.Timeout;
 
 export const RedisClient = createClient({
   socket: {
@@ -17,12 +17,12 @@ export const RedisClient = createClient({
 export const SetupRedisCallbacks = async () => {
   RedisClient.on("error", (err) => {
     if (err.type != SocketClosedUnexpectedlyError) {
-      console.log("Redis Client Error: ", err);
+      console.error("Redis Client Error: ", err);
     }
   });
   RedisClient.on("ready", () => console.log("Redis is ready to serve."));
   RedisClient.on("reconnecting", () =>
-    console.log("Redis disconnected, attempting to reconnect...")
+    console.warn("Redis disconnected, attempting to reconnect...")
   );
   RedisClient.on("connect", () => console.log("Redis is connected."));
   RedisClient.on("end", () => RedisReconnect());
@@ -30,9 +30,9 @@ export const SetupRedisCallbacks = async () => {
 };
 
 const RedisReconnect = async () => {
-  await RedisClient.connect().catch((err) => {
-    if (err.number === -4078 && reconnect === null) {
-      console.log("Unable to connect to redis, trying again in 1 second...");
+  await RedisClient.connect().catch(() => {
+    if (reconnect === undefined) {
+      console.warn("Unable to connect to redis, trying again in 1 second...");
       reconnect = setTimeout(RedisReconnect, 1000);
     }
   });
