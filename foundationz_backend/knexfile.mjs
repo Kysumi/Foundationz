4677ea88b87e9cd850a6816580ab69754766a64e
Abomy <abomyy@gmail.com>
2022-04-04 18:07:51 +1200

    Migrations for allotments and renaming projects done. Tweak to knex config to user common js

--- foundationz_backend/knexfile.mjs
+++ foundationz_backend/knexfile.mjs
@@ -4,19 +4,23 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export default {
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: 'foundationz',
-      user:     'found',
-      password: 'found'
+    development: {
+        client: 'postgresql',
+        connection: {
+            database: 'foundationz',
+            user: 'found',
+            password: 'found'
+        },
+        debug: true,
+        migrations: {
+            directory: "./db/migrations",
+            extension: 'cjs',
+            loadExtensions: ['.cjs']
+        },
+        seeds: {
+            directory: './db/seeds',
+            extension: 'js',
+            loadExtensions: ['.js']
+        }
     },
-    debug: true,
-    migrations: {
-      directory: "./db/migrations",
-    },
-    seeds: {
-      directory: './db/seeds',
-    }
-  },
 };
