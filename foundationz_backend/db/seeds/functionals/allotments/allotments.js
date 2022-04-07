40423312ce9ba339eb814861b92bff8c5454c78c
Abomy <abomyy@gmail.com>
2022-04-07 17:05:00 +1200

    CW-16 Made allotments generate 5 random for day then few others spread out

--- foundationz_backend/db/seeds/functionals/allotments/allotments.js
+++ foundationz_backend/db/seeds/functionals/allotments/allotments.js
@@ -5,26 +5,58 @@ import { faker } from "@faker-js/faker";
 
 export default class AllotmentSeeder extends BaseSeeder {
   generate() {
-    this.data = Bookings.randomList(this.amount).reduce((current, booking) => {
-      const start_date = faker.helpers.randomize([
-        faker.date.past(1),
-        faker.date.soon(10),
-      ]);
-      const end_date = faker.date.future(1);
-      return [
-        ...current,
-        {
-          id: v4(),
-          booking_id: booking.id,
-          organization_id: booking.organization_id,
-          type_id: BookingTypes.random().id,
-          start_date: start_date,
-          end_date: end_date,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ];
-    }, []);
+    const week = Bookings.randomList(this.amount * 4).reduce(
+      (current, booking) => {
+        const start_date = faker.helpers.randomize([
+          faker.date.soon(2),
+          faker.date.soon(10),
+        ]);
+        const end_date = faker.date.future(1);
+        return [
+          ...current,
+          ...this.populateFields(booking, start_date, end_date),
+        ];
+      },
+      []
+    );
+
+    const currentDay = Bookings.randomList(this.amount).reduce(
+      (current, booking) => {
+        const morning = new Date();
+        const evening = new Date();
+
+        morning.setHours(8, this.randomNumberPlox(59), 0, 0);
+        evening.setHours(16, this.randomNumberPlox(59), 0, 0);
+
+        const toady = faker.date.between(
+          morning.toISOString(),
+          evening.toISOString()
+        );
+
+        const end_date = faker.date.soon(1);
+
+        return [...current, ...this.populateFields(booking, toady, end_date)];
+      },
+      []
+    );
+
+    this.data = [...currentDay, ...week];
+
     return this;
+  }
+
+  populateFields(booking, start, end) {
+    return [
+      {
+        id: v4(),
+        booking_id: booking.id,
+        organization_id: booking.organization_id,
+        type_id: BookingTypes.random().id,
+        start_date: start,
+        end_date: end,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
   }
 }
